5dfa65896f61bc3a9d743ef4e9c27d0bf2123562
CeoFred <johnsonmessilo19@gmail.com>
2019-01-18 17:51:00 +0100

    with jwt

--- server/handlers/index.js
+++ server/handlers/index.js
@@ -1,11 +1,15 @@
 const db = require("../models/index");
+const jwt = require('jsonwebtoken');
 
 module.exports.register =  async (req, res, next) => {
   try {
       console.log('body details',req.body);
     const user = await db.User.create(req.body);
-
-    res.status(200).json(user);
+    const {id,username} = {user};
+    const token = jwt.sign({id,username},process.env.SECRET)
+    res.status(200).json(id,
+        username,
+        token);
     
   } catch (error) {
       if(err.code === 11000){
@@ -22,9 +26,12 @@ module.exports.login = async (req, res, next) => {
       const {id,username } = user;
       const valid = await user.comparePassword(req.body.pass);
       if(valid){
+    const token = jwt.sign({id:username},process.env.SECRET)
+
           res.json({
               id,
-              username
+              username,
+              token
           });
       }else{
           throw new Error('Wrong username and password');
